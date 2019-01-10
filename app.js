f747120f0a451775889de8d5967ad72a507aee5f
lisaward50 <lisa.ward50@gmail.com>
2019-01-10 13:59:25 -0500

    add login and logout

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express        = require("express");
 const app            = express();
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
+const flash          = require("connect-flash");
+const passport       = require("passport");
+const LocalStrategy  = require("passport-local");
 const methodOverride = require("method-override");
-const Comment        = require("./models/comment");
 const Post           = require("./models/post");
-const User          = require("./models/user");
+const Comment        = require("./models/comment");
+const User           = require("./models/user");
 const seedDatabase   = require("./seed");
 const commentRoutes  = require("./routes/comments");
 const postRoutes     = require("./routes/posts");
@@ -18,6 +21,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
+
+app.use(require("express-session")({
+  secret: "secret thing",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+})
 
 //seedDatabase();
 
--- routes/index.js
+++ routes/index.js
@@ -17,13 +17,32 @@ router.post("/register", function(req, res){
   const newUser = new User({username: req.body.username});
   User.register(newUser, req.body.password, function(err, user){
     if(err){
-      console.log(err);
+      req.flash("error", err.message);
       return res.redirect("/register");
     }
     passport.authenticate("local")(req, res, function(){
+      req.flash("success", "Welcome to Elevate " + user.username);
       res.redirect("/posts");
     });
   });
+});
+
+//SHOW LOGIN FORM
+router.get("/login", function(req, res){
+  res.render("auth/login");
+});
+
+//HANDLING LOGIN LOGIC/FUNCTIONALITY
+router.post("/login", passport.authenticate("local",
+  { successRedirect: "/posts",
+    failureRedirect: "/login"
+  }), function(req, res){
+});
+
+//LOGOUT ROUTE
+router.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/posts");
 });
 
 module.exports = router;
